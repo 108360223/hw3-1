6eadec2565dede04c553acbbe2d37d2507dc5335
108360223 <55587874+108360223@users.noreply.github.com>
2019-10-30 02:04:58 +0800

    Add files via upload

---
+++ P15/source/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+int maximun(int x, int y, int z);
+int main(void)
+{
+	int n1;
+	int n2;
+	int n3;
+	printf("Enter three integers:");
+	scanf_s("%d %d %d" ,& n1, &n2, &n3);
+	printf("Maximum is :%d\n", maximun(n1, n2, n3));
+	system("pause");
+	return 0;
+}
+int maximun(int x, int y, int z)
+{
+	int max = x;
+	if (y > max)
+	{
+		max = y;
+	}
+	if (z > max) 
+	{
+		max = z;
+	}
+	return max;
+}
